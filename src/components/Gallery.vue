6c4626fa9f7929a1d03b44ac37279bbb1c4ff99c
SeungMin <seungmin.lee816@navercorp.com>
2022-03-09 22:22:28 +0900

    feat: 2018_2019

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -10,15 +10,15 @@
       <div class="row syw-section-content text-center animate__animated animate__slow animate__fadeIn">
         <div class="col-6">
           <img class="w-100 h-75"
-               v-bind:src="require('@/assets/img/hall/1.jpg')"
-               v-on:click="showByYear('2019-2020')"
+               v-bind:src="require('@/assets/img/gallery/2018_2019/MAIN.jpg')"
+               v-on:click="showByYear('2018-2019')"
           />
           <h2>2019 - 2020</h2>
         </div>
         <div class="col-6">
           <img class="w-100 h-75"
                v-bind:src="require('@/assets/img/hall/2.jpg')"
-               v-on:click="showByYear('2021-2022')"
+               v-on:click="showByYear('2020-2021')"
           />
           <h2>2021 - 2022</h2>
         </div>
@@ -73,8 +73,44 @@ export default {
       show();
     };
     const getImagesByYear = (year) => {
-      if (year === '2019-2020') {
-        return [{title: '', src: require("@/assets/img/gallery/year/2019_bride.jpg")}];
+      if (year === '2018-2019') {
+        return [
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20181216_211827.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20181225_182052.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20181225_183304.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20181231_192648.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20181231_215158.jpg")},
+
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190713_211232.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190713_211246.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190713_211253.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190714_203336.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190719_220713.jpg")},
+
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190721_181421.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190721_181431.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190726_103604.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190726_103620.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190726_150521.jpg")},
+
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190816_204335.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190816_222823.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190816_223051.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190830_181016.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190910_094048.jpg")},
+
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190910_211658.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190912_110206.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190912_114134.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190912_124925.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20190921_185108.jpg")},
+
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20191012_165058.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20191012_165101.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20191019_194953.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20191019_200946.jpg")},
+          {title: '', src: require("@/assets/img/gallery/2018_2019/20191228_175101.jpg")},
+        ];
       }
       if (year === '2021-2022') {
         return [{title: '', src: require("@/assets/img/gallery/year/2019_bride.jpg")}];
@@ -109,7 +145,6 @@ export default {
       if (theme === 'travel') {
         return [
           {title: '', src: require("@/assets/img/gallery/theme/tmon_1.jpg")},
-          {title: '', src: require("@/assets/img/gallery/theme/tmon_1.jpg")}
         ];
       }
       return [];
