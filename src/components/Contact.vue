e1301cf13f226d947723c72af2aeb6f1f5a3031c
SeungMin <seungmin.lee816@navercorp.com>
2022-03-13 22:04:11 +0900

    feat: ContactUsDetail 추가

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,14 +1,14 @@
 <template>
-  <contact-us></contact-us>
+  <contact-us-detail></contact-us-detail>
   <wedding-hall></wedding-hall>
 </template>
 <script>
-import ContactUs from "@/components/ContactUs";
+import ContactUsDetail from "@/components/ContactUsDetail";
 import WeddingHall from "@/components/WeddingHall";
 
 export default {
   name: 'Contact',
-  components: {ContactUs, WeddingHall}
+  components: {ContactUsDetail, WeddingHall}
 };
 </script>
 
