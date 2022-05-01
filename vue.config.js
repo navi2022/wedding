fad1e6a911fa2e03d8754f0f58f806973f9917ed
SeungMin <seungmin.lee816@navercorp.com>
2022-05-01 15:36:04 +0900

    style: bundle analyzer & compression 추가

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const zlib = require("zlib");
 module.exports = {
     lintOnSave: false,
     publicPath: process.env.NODE_ENV === 'production'
@@ -12,12 +14,48 @@ module.exports = {
             hints: false
         },
         optimization: {
+            runtimeChunk: true,
             splitChunks: {
-                minSize: 10000,
-                maxSize: 250000,
+                chunks: 'all',
+                cacheGroups: {
+                    vendor: {
+                        chunks: 'initial',
+                        name: 'vendor',
+                        enforce: true,
+                    },
+                },
+            },
+        },
+        plugins: [
+            new BundleAnalyzerPlugin()
+        ],
+    },
+    pluginOptions: {
+        webpackBundleAnalyzer: {
+            openAnalyzer: false, // 자동으로 open하지 않음
+            analyzerMode: 'static' /* 분석파일 html 보고서를 dist 폴더에 저장한다  */,
+            reportFilename: 'bundle-report.html', // 리포트이름
+        },
+        compression:{
+            brotli: {
+                filename: '[file].br[query]',
+                algorithm: 'brotliCompress',
+                include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+                compressionOptions: {
+                    params: {
+                        [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+                    },
+                },
+                minRatio: 0.8,
+            },
+            gzip: {
+                filename: '[file].gz[query]',
+                algorithm: 'gzip',
+                include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+                minRatio: 0.8,
             }
         }
-    }
+    },
 }
 
 
